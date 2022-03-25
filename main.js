babfa8f68a8bf0222297681c188375e1dc6b50ec
AntoineMairesse <45721024+AntoineMairesse@users.noreply.github.com>
2022-03-25 18:25:53 +0100

    fix detailed skins

--- main.js
+++ main.js
@@ -41,6 +41,39 @@ client.on('ready', () => {
     console.log('Ready')
 })
 
+async function mask() {
+
+    const image = await Jimp.read('test.png');
+    const watermark = await Jimp.read('mask.png');
+
+    image.composite(watermark, 0, 0, {
+        mode: Jimp.BLEND_SOURCE_OVER
+    })
+    await image.writeAsync('masked.png');
+
+    await Jimp.read('masked.png').then(image => {
+        const targetColor = { r: 0, g: 255, b: 0, a: 255 }; // Color you want to replace
+        const replaceColor = { r: 0, g: 0, b: 0, a: 0 }; // Color you want to replace with
+        const colorDistance = (c1, c2) => Math.sqrt(Math.pow(c1.r - c2.r, 2) + Math.pow(c1.g - c2.g, 2) + Math.pow(c1.b - c2.b, 2) + Math.pow(c1.a - c2.a, 2)); // Distance between two colors
+        const threshold = 32; // Replace colors under this threshold. The smaller the number, the more specific it is.
+        image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+            const thisColor = {
+                r: image.bitmap.data[idx + 0],
+                g: image.bitmap.data[idx + 1],
+                b: image.bitmap.data[idx + 2],
+                a: image.bitmap.data[idx + 3]
+            };
+            if (colorDistance(targetColor, thisColor) <= threshold) {
+                image.bitmap.data[idx + 0] = replaceColor.r;
+                image.bitmap.data[idx + 1] = replaceColor.g;
+                image.bitmap.data[idx + 2] = replaceColor.b;
+                image.bitmap.data[idx + 3] = replaceColor.a;
+            }
+        });
+        image.write('test.png');
+    });
+}
+
 async function crop(message) {
     if (sizeOf('test.png').height == 32) {
         let watermark = await Jimp.read("test.png");
@@ -51,6 +84,8 @@ async function crop(message) {
         })
         await image.writeAsync('test.png');
     }
+
+    await mask()
 
     let watermark = await Jimp.read("1.png");
 
